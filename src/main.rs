067e7eb29a246647a11711fab3bdde2438aa0d20
gwy15 <gwy15thu@gmail.com>
2020-07-05 23:17:39 +0800

    完善遍历文件

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::path::PathBuf;
+
 #[macro_use]
 extern crate log;
-
 use glob::glob;
 
 mod model;
@@ -13,20 +14,43 @@ fn init() {
     info!("初始化完成，当前版本: {}", env!("CARGO_PKG_VERSION"));
 }
 
+fn file_finder() -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let paths = ["challenge", "dealto", "spy"];
+    let mut files = vec![];
+    for path in paths.iter() {
+        let pattern = format!("{}/*.json", path);
+        let msg = format!("Failed to glob files in {}", path);
+        for entry in glob(&pattern).expect(&msg) {
+            let file = entry?;
+            files.push(file);
+        }
+    }
+    Ok(files)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     init();
 
     // find files
     let mut wars = vec![];
-    for entry in glob("challenge/*.json").expect("Failed to glob files") {
-        let path = entry?;
+    for path in file_finder()? {
         let war = utils::parse(path)?;
         wars.push(war);
     }
+    log::info!("解析了 {} 个文件", wars.len());
 
     // write
+    log::debug!("开始输出");
     let mut writer = writer::Writer::new();
     writer.write(wars);
+    log::info!("完成输出");
+
+    // wait for key
+    let mut _buf = String::new();
+    println!("回车打钱");
+    std::io::stdin()
+        .read_line(&mut _buf)
+        .expect("读取 stdin 错误");
 
     Ok(())
 }
